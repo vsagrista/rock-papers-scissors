3eeef905d20e4f4e0fcc351accf0619f8297bd8c
victor sagrista <victorsagristalopez@gmail.com>
2022-08-14 10:26:13 +0200

    add sound

--- scripts/game.js
+++ scripts/game.js
@@ -4,7 +4,7 @@ const lossText = ["Bummer, try again", "You lost!", "Computer gets the point!"];
 const drawText = "It's a tie!";
 const gamePrompt = "> Rock, paper, scissors! Please, enter your choice:";
 const errorMessage = "Please, type 'rock', 'paper' or 'scissor and hit enter";
-let computerSelection, userSelection;
+let computerSelection, userSelection, audio;
 let computerPoints = 0;
 let userPoints = 0;
 let pointsWinner = "";
@@ -64,6 +64,14 @@ function printWarning(result) {
     }
 }
 
+function playAudio(type) {
+    if(audio) {
+        audio.pause();
+    }
+    audio = new Audio(`./sounds/${type}-1.mp3`);
+    audio.play();
+}
+
 function manageScore() {
     changeText("computer-points", computerPoints);
     changeText("user-points",userPoints);
@@ -74,18 +82,27 @@ function manageScore() {
         flashScore("user-points", "highlight-blue");
         flashScore("show-winner-scores-board-all", "blue-background");
         flashScore("print-text", "blue-background");
+        playAudio("win");
     } else if(pointsWinner === "computer") {
         printWarning("computer");
         flashScore(`action-button-${userSelection}`, "highlight-red");
         flashScore("computer-points", "highlight-red");
         flashScore("show-winner-scores-board-all", "red-background");
         flashScore("print-text", "red-background");
+        playAudio("fail");
     } else {
         printWarning("draw");
         flashScore("scores-board-all", "highlight-all");
         flashScore("show-winner-scores-board-all", "orange-background");
         flashScore("print-text", "orange-background");
+        playAudio("draw");
     }
+}
+
+function stopAllAudio(){
+	allAudios.forEach(function(audio){
+		audio.pause();
+	});
 }
 
 function flashScore(itemToHighlight, className) {
@@ -116,7 +133,7 @@ function getPlayersCards() {
 }
  
 function runGame() {
-
+    
     document.getElementById("action-button-rock").addEventListener("click", getPlayersCards);
     document.getElementById("action-button-paper").addEventListener("click", getPlayersCards);
     document.getElementById("action-button-scissors").addEventListener("click", getPlayersCards);
